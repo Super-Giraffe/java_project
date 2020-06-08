31e99dbd385c194d26db87310824def1a01155b8
Super-Giraffe <1057157862@qq.com>
2020-06-08 16:03:21 +0800

    从上到下打印二叉树II

--- Solution29.java
+++ Solution29.java
@@ -3,10 +3,10 @@ package LeetCode;
 import java.util.*;
 
 /**
- * 题目：从上到下打印二叉树II
- * 从上到下按层打印二叉树，同一层的节点按从左到右的顺序打印，每一层打印到一行。
+ * 题目：从上到下打印二叉树III
+ * 从上到下按层打印二叉树，，每一层打印到一行。
  */
-public class Solution28 {
+public class Solution29 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> res = new ArrayList<>();
@@ -19,6 +19,8 @@ public class Solution28 {
             int nowNum = 1;
             //nextNum为下一层的节点数
             int nextNum = 0;
+            //layer为当前层的层数
+            int layer = 1;
             while (!queue.isEmpty()){
                 TreeNode node = queue.poll();
                 list.add(node.val);
@@ -33,10 +35,23 @@ public class Solution28 {
                 nowNum--;
                 //当当前层的节点都遍历完后进入下一层，即将下一层的节点数设置为当前层的节点数，下一层节点数清零，并重新创建一个Lits记录数据
                 if (nowNum == 0){
+                    if (layer % 2 == 0){
+//                        int i = 0;
+//                        int j = list.size() - 1;
+//                        while (i < j){
+//                            int temp = list.get(i);
+//                            list.set(i, list.get(j));
+//                            list.set(j, temp);
+//                            i++;
+//                            j--;
+//                        }
+                        Collections.reverse(list);
+                    }
                     res.add(list);
                     list = new ArrayList<>();
                     nowNum = nextNum;
                     nextNum = 0;
+                    layer++;
                 }
             }
         }
