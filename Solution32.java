43523a3011eb26aaa9095d16bd21fc749f868a27
Super-Giraffe <1057157862@qq.com>
2020-06-18 15:26:12 +0800

    最长公共前缀

--- Solution32.java
+++ Solution32.java
@@ -29,30 +29,3 @@ class Solution32 {
         return str1.substring(0, index);
     }
 }
-/*
-* 运行时间更短的写法
-* 思路：边界情况有两种，长度为1和2的子串，通过向两边扩展的做法求最长回文子串
-* public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);//长度为1的子串扩展
-            int len2 = expandAroundCenter(s, i, i + 1);//长度为2的子串扩展
-            int len = Math.max(len1, len2);//获取两种扩展情况的最大长度
-            if (len > end - start) {//判断长度是否大于当前最长回文子串的长度
-                start = i - (len - 1) / 2;//获取起始坐标
-                end = i + len / 2;获取结束坐标
-            }
-        }
-        return s.substring(start, end + 1);//通过两个坐标返回最长回文子串
-    }
-
-    private int expandAroundCenter(String s, int left, int right) {
-        int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
-        }
-        return R - L - 1;
-    }
-* */
