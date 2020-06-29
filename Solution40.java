692a5017b16888a338a6aae8ce003a3673a91254
Super-Giraffe <1057157862@qq.com>
2020-06-29 10:17:29 +0800

    数组中的第k个最大元素，不去重

---
+++ Solution40.java
@@ -0,0 +1,64 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 题目：数组中的第k个最大元素，不去重
+ */
+
+public class Solution40 {
+    public int findKthLargest(int[] nums, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            list.add(nums[i]);
+        }
+        Collections.sort(list);
+        return list.get(nums.length - k);
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{3,2,3,1,2,4,5,5,6};
+        int res = new Solution40().findKthLargest(nums, 4);
+        System.out.println(res);
+    }
+}
+/*
+    堆排序的解法：
+    public int findKthLargest(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            swap(nums, 0, i);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    public void buildMaxHeap(int[] a, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(a, i, heapSize);
+        }
+    }
+
+    public void maxHeapify(int[] a, int i, int heapSize) {
+        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
+        if (l < heapSize && a[l] > a[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(a, i, largest);
+            maxHeapify(a, largest, heapSize);
+        }
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+*/
